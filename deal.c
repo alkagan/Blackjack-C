6502ee447893a41a85389291e5d533d9f5921e63
Alex <alkagan@ucsc.edu>
2018-11-30 15:23:56 -0800

    looping functionality almost working properly

--- deal.c
+++ deal.c
@@ -4,7 +4,8 @@
 #include "deck.h"
 #include "players.h"
 
-static int card_counter = 0; 
+static int card_counter = 0;
+static int flag;
 
 void deal(struct Deck *thisDeck){
     int player = 0;
@@ -14,6 +15,8 @@ void deal(struct Deck *thisDeck){
     pPlayer = &player;
     pDealer = &dealer;
     char input;
+    card_counter = 0;
+    flag = 0;
 
     shuffle(thisDeck);
     printf("Dealing\n");
@@ -25,7 +28,7 @@ void deal(struct Deck *thisDeck){
     printf("%d %d\n", thisDeck->cards[1], thisDeck->cards[3]);
     printf("Dealer initial total value is: %d\n", dealer);
 
-    while (1){
+    while (!flag){
         printf("Hit (h) or Stay (s)?\n");
         scanf("%c", &input);
         
@@ -35,9 +38,23 @@ void deal(struct Deck *thisDeck){
             stay(thisDeck, pDealer, pPlayer);
         } else if (input == 'x'){
             return;
-        } else {
-            printf("Please try again\n");
         }
+
+        // if (flag){
+        //     break;
+        // }
+    }
+}
+
+void decision(struct Deck *thisDeck){
+    char input;
+    printf("Game over, continue playing?\n(y) to continue or any key to exit\n");
+    scanf("%c", &input);
+    if (input == 'y' || input == 'Y'){
+        deal(thisDeck);
+        flag = 1;
+    } else {
+        return;
     }
 }
 
@@ -45,11 +62,12 @@ void hit(struct Deck *thisDeck, int *p){
     printf("%d\n", thisDeck->cards[4+card_counter]);
     *p += thisDeck->cards[4+card_counter];
     printf("Player value is %d\n", *p);
-    if (*p > 21){
-        printf("You lose\n");
-    }
     card_counter++;
     printf("Card_counter in hit(): %d\n", card_counter);
+    if (*p > 21){
+        printf("You lose!\n");
+        decision(thisDeck);
+    }
 }
 
 void stay(struct Deck *thisDeck, int *pD, int *pP){
@@ -58,14 +76,16 @@ void stay(struct Deck *thisDeck, int *pD, int *pP){
         *pD += thisDeck->cards[4+card_counter];
         if (*pD > 21){
             printf("Dealer busts, you win!\n");
+            break;
         }
         card_counter++;
         printf("Card_counter in stay(): %d\n", card_counter);
     }
 
-    if (*pD >= *pP){
+    if (*pD >= *pP && *pD <= 21){
         printf("Dealer wins!\n Dealer: %d\nYou: %d\n", *pD, *pP);
     } else {
         printf("You win!\n Dealer: %d\nYou: %d\n", *pD, *pP);
     }
+    decision(thisDeck);
 }
